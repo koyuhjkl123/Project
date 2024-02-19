921bd549bc7368c6d6da461261f79af8dca02767
SQL <koyu12>
2024-02-20 08:59:31 +0900

    2024-02-16

---
+++ big-colorful-toggle-menucss-only-cpc/dist/intor.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+  document.getElementById('nav-toggle').checked = true;
+}, 1000); // 1000ms = 1초
--- js/main.js
+++ js/main.js
@@ -1,35 +1,100 @@
-// JavaScript 코드
-document.addEventListener('DOMContentLoaded', function () {
-  let myPageMenu = document.querySelector('.top-menu .menu-list li:nth-child(3)');
+function toggleMenu() {
+  let menuContainer = document.getElementById("menu-container");
+  // 현재 투명도 값 가져오기
+  let currentOpacity = parseFloat(window.getComputedStyle(menuContainer).opacity);
 
-  myPageMenu.addEventListener('mouseenter', function () {
-    myPageMenu.querySelector('ul').style.opacity = '1';
-    myPageMenu.querySelector('ul').style.transform = 'translateY(0)';
-  });
-
-  myPageMenu.addEventListener('mouseleave', function () {
-    myPageMenu.querySelector('ul').style.opacity = '0';
-    myPageMenu.querySelector('ul').style.transform = 'translateY(-40px)';
-  });
-});
+  // 투명도가 0이면 1로, 1이면 0으로 변경
+  menuContainer.style.opacity = currentOpacity === 0 ? 1 : 0;
+  // 클릭 가능 여부 설정
+  menuContainer.style.pointerEvents = currentOpacity === 0 ? "auto" : "none";
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-  const icon = document.querySelector('.icon');
-  const menubar = document.querySelector('.menubar');
+  let iconContainer = document.querySelector('#container header .icon-container');
+  let menubar = document.querySelector('.menubar');
+  let icon = document.querySelector('.icon');
+  let menuUl = document.querySelector('.menubar #menu-container ul');
+  let menuSubUl = document.querySelector('header .menubar #menu-container ul li > ul');
 
-  icon.addEventListener('click', function () {
+  iconContainer.addEventListener('click', function () {
+    iconContainer.classList.toggle('active');
     menubar.classList.toggle('active');
+
+    if (iconContainer.classList.contains('active')) {
+      menubar.style.left = '-150px'; // 클릭 후
+      icon.style.display = 'none'; // 햄버거 제거
+    } else {
+      menubar.style.left = '-1220px'; // 클릭 전
+      icon.style.display = 'block'; // 햄버거 나타내기
+    }
+
+    if (iconContainer.classList.contains('active')) {
+      menuUl.style.opacity = '1'; // 클릭 후
+      menuSubUl.style.opacity = '1';
+    } else {
+      menuUl.style.opacity = '0'; // 클릭 전
+    }
   });
 });
 
-document.addEventListener('DOMContentLoaded', function(){
-  const icon = document.querySelector('.icon');
-  const menuUl = document.querySelector('header .menubar');
 
-  icon.addEventListener('click', function(){
-    menuUl.querySelector('ul').style.opacity = '1';
-    menuUl.querySelector('ul').style.transition = '1.2s';
-    menuUl.querySelector('ul').style.transform = 'translateY(-20px)';
+// 메뉴바 x클릭 시 메뉴바 닫기
+document.addEventListener('DOMContentLoaded', function() {
+  let closeButton = document.querySelector('.close');
+  let menuContainer = document.querySelector('.menubar #menu-container ul');
+
+  // 이미지가 클릭될 때 이벤트 핸들러 추가
+  closeButton.addEventListener('click', function() {
+
+    if (iconContainer.classList.contains('active')) {
+      menubar.style.left = '-1220px'; // 클릭 후
+    } else {
+      menubar.style.left = '0'; // 클릭 전
+    }
+    menuContainer.classList.remove('active'); // 'active' 클래스 제거
+
+    
+  });
+});
+// 메뉴 클릭 시 --> 각 li 아이템들 hover 시 동작
+document.addEventListener('DOMContentLoaded', function () {
+  let menuItems = document.querySelectorAll('header .menubar #menu-container ul > li a');
+
+  // 각 메뉴 아이템에 이벤트 리스너 추가
+  menuItems.forEach(function (item) {
+    item.addEventListener('mouseover', function () {
+      // 마우스가 요소 위로 이동할 때 수행할 작업
+      item.style.color = 'black';
+      item.style.opacity = '0.5';
+    });
+
+    item.addEventListener('mouseout', function () {
+      // 마우스가 요소에서 벗어날 때 수행할 작업
+      item.style.color = '';
+      item.style.opacity = '1';
+    });
+  });
+});
+
+let scrollHeight = Math.max(
+  document.body.scrollHeight, document.documentElement.scrollHeight,
+  document.body.offsetHeight, document.documentElement.offsetHeight,
+  document.body.clientHeight, document.documentElement.clientHeight
+);
+
+alert('스크롤에 의해 가려진 분을 포함한 전체 문서 높이: ' + scrollHeight);
+
+function scroll() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop || window.screenY;
+
+  document.querySelectorAll(".content__item").forEach(item => {
+    if (scrollTop > item.offsetTop - window.innerHeight / 2) {
+      item.classList.add("show");
+    }
   });
 
-});
+  // 1분에 60번 실행
+  // 브라우저에 최적화 부드럽게, 메모리 훨씬 더 적게 차지
+  requestAnimationFrame(scroll);
+}
+scroll();
